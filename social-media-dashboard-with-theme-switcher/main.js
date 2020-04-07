02cac2cb3ffb95bb401214e9f30243522201b3c0
NetanelK <kadoshnetanel@gmail.com>
2020-04-07 22:55:16 +0300

    Add files via upload

---
+++ social-media-dashboard-with-theme-switcher/main.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+  var toggle = document.getElementById("chk");
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      document.documentElement.style.setProperty("--bg", "hsl(230, 17%, 14%)");
+      document.documentElement.style.setProperty(
+        "--bg-p",
+        "hsl(232, 19%, 15%)"
+      );
+      document.documentElement.style.setProperty(
+        "--card-bg",
+        "hsl(228, 28%, 20%)"
+      );
+      document.documentElement.style.setProperty("--card-hover", "#2e3552");
+      document.documentElement.style.setProperty("--h1", "white");
+      document.documentElement.style.setProperty("--h2", "white");
+      document.documentElement.style.setProperty(
+        "--span",
+        "hsl(228, 34%, 66%)"
+      );
+    } else {
+      document.documentElement.style.setProperty("--bg", "white");
+      document.documentElement.style.setProperty(
+        "--card-bg",
+        "hsl(227, 47%, 96%)"
+      );
+      document.documentElement.style.setProperty("--card-hover", "#e5e8f6");
+      document.documentElement.style.setProperty("--h1", "black");
+      document.documentElement.style.setProperty("--h2", "hsl(228, 12%, 44%)");
+      document.documentElement.style.setProperty(
+        "--span",
+        "hsl(228, 12%, 44%)"
+      );
+    }
+  });
+};
